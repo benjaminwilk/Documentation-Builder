e8b0556247ef3883e09178301610f1bd90022e85
Benjamin Wilk <benjaminawilk@gmail.com>
2019-03-19 14:58:42 -0400

    Add project files.

---
+++ DocumentationBuilder/DocumentStripper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentationBuilder {
+    class DocumentStripper {
+
+        private char vertIcon;
+        private char horiIcon;
+        private char crosIcon;
+        private int typeWidth;
+        private int methodWidth;
+
+        String[] userText;
+
+        public DocumentStripper(String inputText, String vertIcon, String horiIcon, String crosIcon, String typeWidth, String methodWidth) {
+            this.vertIcon = Char.Parse(vertIcon);
+            this.horiIcon = Char.Parse(horiIcon);
+            this.crosIcon = Char.Parse(crosIcon);
+            this.typeWidth = int.Parse(typeWidth);
+            this.methodWidth = int.Parse(methodWidth);
+            SplitInputText(inputText);
+        }
+
+        public void SplitInputText(String inputText) {
+            //Array rawInputText;
+            userText = inputText.Split('\n');
+        }
+
+        private void ParseUserText() {
+
+        }
+
+        private String GetVerticalLine() {
+            return "" + this.vertIcon;
+        }
+
+        private String GetHorizontalLine() {
+            StringBuilder horizontalLine = new StringBuilder();
+            horizontalLine.Append(this.crosIcon);
+            for (int i = 0; i < typeWidth; i++) {
+                horizontalLine.Append(this.horiIcon);
+            }
+            horizontalLine.Append(this.crosIcon);
+            for (int i = 0; i < methodWidth; i++) {
+                     horizontalLine.Append(this.horiIcon);
+            }
+            horizontalLine.Append(this.crosIcon + "\n");
+            return horizontalLine.ToString();
+        }
+
+        private String LeftAlignmentTextWithPadding(String passedText, int TypeOrMethod) {
+            StringBuilder leftAlign = new StringBuilder();
+            int spacingCount = TypeOrMethod - passedText.Length;
+            leftAlign.Append(passedText);
+            for (int i = spacingCount; i < TypeOrMethod; i++) {
+                leftAlign.Append(" ");
+            }
+            return leftAlign.ToString();
+        }
+
+        private String GetConstructorSummaryHeader() {
+            StringBuilder constructorHeader = new StringBuilder();
+            constructorHeader.Append();
+        }
+
+        private String GetMethodSummaryHeader() {
+            StringBuilder headerMessage = new StringBuilder();
+            headerMessage.Append(GetHorizontalLine());
+            headerMessage.Append(GetVerticalLine() + LeftAlignmentTextWithPadding("Modifier and Type", this.typeWidth) + GetVerticalLine() + LeftAlignmentTextWithPadding("Method and Description", this.methodWidth) + GetVerticalLine());
+            headerMessage.Append(GetHorizontalLine());
+            return headerMessage.ToString();
+        }
+
+        public String DisplayText() {
+            StringBuilder sb =  new StringBuilder();
+            sb.Append(GetMethodSummaryHeader());
+            for (int i = 0; i < userText.Length; i++) {
+                sb.Append(userText[i]);
+            }
+            return sb.ToString();
+        }
+
+    }
+
+
+
+}
---
+++ DocumentationBuilder/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DocumentationBuilder {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void MenuOptionButton_Click(object sender, RoutedEventArgs e) {
+            OptionsWindow ow = new OptionsWindow();
+            ow.Show();
+        }
+
+        private void MenuCloseButton_Click(object sender, RoutedEventArgs e) {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void Expander_Expanded(object sender, RoutedEventArgs e) {
+            SizeToContent = SizeToContent.Height + 50;
+        }
+
+        private void ComputationButton_Click(object sender, RoutedEventArgs e) {
+            OptionsWindow ow = new OptionsWindow();
+            DocumentStripper ds = new DocumentStripper(InputBox.Text, ow.OptionsVerticalIcon.Text, ow.OptionsHorizontalIcon.Text, ow.OptionsCrossIcon.Text, ow.OptionsTypeWidthBox.Text, ow.OptionsMethodWidth.Text);
+            this.InputBox.Text = String.Empty;
+            InputBox.AppendText(ds.DisplayText().ToString());
+        }
+    }
+
+}
